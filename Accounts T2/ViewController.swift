6805175f3dc37291f0ece4c9c7841695a7137bea
JunYi <junyi@saws-macbook-air.local>
2018-04-09 11:10:05 +0800

    Initial Commit

---
+++ Accounts T2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Accounts T2
+//
+//  Created by JunYi on 9/4/18.
+//  Copyright © 2018 dzhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
