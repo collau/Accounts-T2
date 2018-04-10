5666b693411195362683bb5792788e52a94d2115
JunYi <junyi@saws-macbook-air.local>
2018-04-10 18:28:34 +0800

    minor changes to account objects

--- AccountEx/BankAccount.swift
+++ AccountEx/BankAccount.swift
@@ -14,7 +14,7 @@ public class BankAccount {
         return [
             INPaymentAccount(nickname: INSpeakableString(spokenPhrase: "Frankie"), number: "******789", accountType: .saving, organizationName: INSpeakableString(spokenPhrase: "AYZ"), balance: INBalanceAmount(amount: 4893.93, currencyCode: "USD"),secondaryBalance: nil),
             INPaymentAccount(nickname: INSpeakableString(spokenPhrase: "JKK Investment"), number: "******321", accountType: .investment, organizationName: INSpeakableString(spokenPhrase: "AYZ"), balance: INBalanceAmount(amount: 1111.93, currencyCode: "GBP"),secondaryBalance: nil),
-            INPaymentAccount(nickname: INSpeakableString(vocabularyIdentifier: "BNM Everyday", spokenPhrase: "BNM Everyday", pronunciationHint: "BNM Everyday"), number: "******482", accountType: .debit, organizationName: INSpeakableString(spokenPhrase: "AYZ"), balance: INBalanceAmount(amount: 88123.93, currencyCode: "KRW"),secondaryBalance: nil),
+            INPaymentAccount(nickname: INSpeakableString(vocabularyIdentifier: "BNM Everyday", spokenPhrase: "BNM Everyday", pronunciationHint: "bee an am Everyday"), number: "******482", accountType: .debit, organizationName: INSpeakableString(spokenPhrase: "AYZ"), balance: INBalanceAmount(amount: 88123.93, currencyCode: "KRW"),secondaryBalance: nil),
             INPaymentAccount(nickname: INSpeakableString(vocabularyIdentifier: "Bobbie", spokenPhrase: "Bobbie", pronunciationHint: "Bobbie"), number: nil, accountType: .debit, organizationName: INSpeakableString(spokenPhrase: "AYZ"), balance: INBalanceAmount(amount: 1234.56, currencyCode: "SGD"),secondaryBalance: INBalanceAmount(amount: 2974, balanceType: .miles))
         ]
     }
